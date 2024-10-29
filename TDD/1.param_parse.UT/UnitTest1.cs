a25068db6496eca33937e207284cfb4ace26520e
Zhi.Xin <zhi.xin@myob.com>
2024-10-29 16:26:46 +0800

    10.29

--- TDD/1.param_parse.UT/UnitTest1.cs
+++ TDD/1.param_parse.UT/UnitTest1.cs
@@ -38,10 +38,35 @@ public class UnitTest1
     }
 
     [Fact]
+    public void Should_throw_error_when_logging_format_error()
+    {
+        Assert.Throws<ArgumentException>(() => Args.Parse("-l l"));
+    }
+
+    [Fact]
     public void Should_parse_port_correctly_when_exist()
     {
         Options options = Args.Parse("-l -p 8080");
         Assert.Equal(8080, options.port);
+    }
+
+    [Fact]
+    public void Should_throw_error_when_port_format_error()
+    {
+        Assert.Throws<ArgumentException>(() => Args.Parse("-p 8080 8081"));
+    }
+
+    [Fact]
+    public void Should_parse_directory_correctly_when_exist()
+    {
+        Options options = Args.Parse("-l -p 8080 -d /usr/tmp");
+        Assert.Equal("/usr/tmp", options.directory);
+    }
+    
+    [Fact]
+    public void Should_throw_error_when_directory_format_error()
+    {
+        Assert.Throws<ArgumentException>(() => Args.Parse("-d /usr /tmp"));
     }
 
 }
--- TDD/1.param_parse/Args.cs
+++ TDD/1.param_parse/Args.cs
@@ -3,16 +3,64 @@ public class Args
   public static Options Parse(string args)
   {
     var argList = args.Split("-").ToList();
-    var loggingOption = argList.Find(a => a.StartsWith("l"));
+    bool logging = ParseLogging(argList);
+    int? port = ParsePort(argList);
 
+    string? dir = ParseDirectory(argList);
+
+    return new Options(logging, port, dir);
+  }
+
+  private static string? ParseDirectory(List<string> argList)
+  {
+    var dirOption = argList.Find(a => a.StartsWith("d"));
+    string? dir = null;
+    if (!string.IsNullOrEmpty(dirOption))
+    {
+      var dirSplit = dirOption.Split(" ");
+
+      if (dirSplit.Length > 2 && !string.IsNullOrEmpty(dirSplit[2]))
+      {
+        throw new ArgumentException(dirOption);
+      }
+      dir = dirSplit[1];
+    }
+
+    return dir;
+  }
+
+  private static int? ParsePort(List<string> argList)
+  {
     var portOption = argList.Find(a => a.StartsWith("p"));
     int? port = null;
     if (!string.IsNullOrEmpty(portOption))
     {
-      var portString = portOption.Split(" ")[1];
-      port = int.Parse(portString);
+      var portSplit = portOption.Split(" ");
+
+      if (portSplit.Length > 2 && !string.IsNullOrEmpty(portSplit[2]))
+      {
+        throw new ArgumentException(portOption);
+      }
+      port = int.Parse(portSplit[1]);
     }
 
-    return new Options(!string.IsNullOrEmpty(loggingOption), port, "");
+    return port;
+  }
+
+  private static bool ParseLogging(List<string> argList)
+  {
+    var loggingOption = argList.Find(a => a.StartsWith("l"));
+    bool logging = false;
+    if (!string.IsNullOrEmpty(loggingOption))
+    {
+      var loggingSplit = loggingOption.Split(" ");
+      if (loggingSplit.Length > 1 && !string.IsNullOrEmpty(loggingSplit[1]))
+      {
+        throw new ArgumentException(loggingOption);
+      }
+      logging = true;
+    }
+
+    return logging;
   }
 }
