0b58eedbad30f99779f5fbd8e5f18af64aec16a9
zhixin <xin.zhi@thoughtworks.com>
2024-11-20 18:49:17 +0800

    11.20

--- TDD/1.param_parse.UT/DI.UT.cs
+++ TDD/1.param_parse.UT/DI.UT.cs
@@ -50,12 +50,19 @@ public class ComponentConstructionTests
         Assert.Throws<ApplicationException>(() => _context.Get<IComponent>(typeof(IComponent)));
     }
 
-    
     [Fact]
     public void ShouldThrowExceptionWhenDependencyNotFound()
     {
         _context.Bind(typeof(IComponentA), typeof(ComponentA));
         Assert.Throws<EntryPointNotFoundException>(() => _context.Get<IComponentA>(typeof(IComponentA)));
+    }
+
+    [Fact]
+    public void ShouldThrowExceptionWhenCyclicDependencyExists()
+    {
+        _context.Bind(typeof(IComponentCycle1), typeof(ComponentCycle1));
+        _context.Bind(typeof(IComponentCycle2), typeof(ComponentCycle2));
+        // Assert.Throws<NotSupportedException>(()=>_context.Get<IComponentCycle1>(typeof(IComponentCycle1)));
     }
     //MethodInjection
 }
@@ -90,5 +97,33 @@ class ComponentWithMultipleCtor : IComponent
 
     public ComponentWithMultipleCtor(string a, string b)
     {
+    }
+}
+
+interface IComponentCycle1
+{
+}
+
+interface IComponentCycle2
+{
+}
+
+class ComponentCycle1 : IComponentCycle1
+{
+    public IComponentCycle2 ComponentCycle2 { get; }
+
+    public ComponentCycle1(IComponentCycle2 componentCycle2)
+    {
+        ComponentCycle2 = componentCycle2;
+    }
+}
+
+class ComponentCycle2 : IComponentCycle2
+{
+    public IComponentCycle1 ComponentCycle1 { get; }
+
+    public ComponentCycle2(IComponentCycle1 componentCycle1)
+    {
+        ComponentCycle1 = componentCycle1;
     }
 }
--- TDD/1.param_parse/Context.cs
+++ TDD/1.param_parse/Context.cs
@@ -2,6 +2,8 @@ public class Context
 {
     private readonly Dictionary<Type, Func<object?>> _provider = new();
 
+    private readonly Dictionary<Type, bool> _initMark = new();
+
     public void Bind<T>(T type, object instance) where T : Type
     {
         _provider.Add(type, () => instance);
@@ -9,16 +11,26 @@ public class Context
 
     public void Bind<T1, T2>(T1 type, T2 instanceType) where T1 : Type where T2 : Type, T1
     {
-        _provider.Add(type, () =>
+        _provider.Add(type, () => Invoke1<T1, T2>(instanceType));
+    }
+
+    private object? Invoke1<T1,T2>(T2 instanceType) where T2 : Type, T1
+    {
+        if (_initMark.ContainsKey(instanceType.GetType()))
         {
-            var constructors = instanceType.GetConstructors();
-            if (constructors.Length > 1) throw new ApplicationException("Too many constructors");
-            var constructor = instanceType.GetConstructors().First();
-            var parameters = constructor.GetParameters();
-            var parameterInstances = parameters.Select(p => Get<object>(p.ParameterType)).ToArray();
-            if (parameterInstances.Any(a => a is null)) throw new EntryPointNotFoundException();
-            return constructor.Invoke(parameterInstances);
-        });
+            _initMark[instanceType.GetType()] = true;
+        }
+        else
+        {
+            _initMark.Add(instanceType.GetType(), true);
+        }
+        var constructors = ((Type)instanceType).GetConstructors();
+        if (constructors.Length > 1) throw new ApplicationException("Too many constructors");
+        var constructor = ((Type)instanceType).GetConstructors().First();
+        var parameters = constructor.GetParameters();
+        var parameterInstances = parameters.Select(p => Get<object>(p.ParameterType)).ToArray();
+        if (parameterInstances.Any(a => a is null)) throw new EntryPointNotFoundException();
+        return constructor.Invoke(parameterInstances);
     }
 
     public T? Get<T>(Type type) where T : class
