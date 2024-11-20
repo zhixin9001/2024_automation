a27548de05f1210f6c8157bf39829c631ae8fdec
Zhi.Xin <zhi.xin@myob.com>
2024-11-20 15:31:18 +0800

    11.20

---
+++ TDD/1.param_parse.UT/Container.UT.cs
@@ -0,0 +1,46 @@
+﻿namespace _1.param_parse.UT;
+
+public class Container_UT
+{
+    private Container _container;
+
+    public Container_UT()
+    {
+        _container = new Container();
+    }
+
+    [Fact]
+    public void ShouldBindTypeToAClassWithDefaultCtor()
+    {
+        _container.Add<IComponent, Component>();
+        var instance = _container.Get<IComponent>();
+        Assert.NotNull(instance);
+        Assert.IsType<Component>(instance);
+    }
+
+    [Fact]
+    public void ShouldThrowExceptionWhenMultipleCtor()
+    {
+        _container.Add<IComponent, ComponentWithMultipleCtor>();
+        Assert.Throws<ApplicationException>(() => _container.Get<IComponent>());
+    }
+
+    [Fact]
+    public void ShouldBindTypeToAClassWithDependency()
+    {
+        _container.Add<IComponent, Component>();
+        _container.Add<IComponentA, ComponentA>();
+        var instance = _container.Get<IComponentA>();
+        Assert.NotNull(instance);
+        Assert.IsType<ComponentA>(instance);
+        Assert.NotNull(((ComponentA)instance).Component);
+        Assert.IsType<Component>(((ComponentA)instance).Component);
+    }
+
+    [Fact]
+    public void ShouldThrowExceptionWhenDependencyNotFound()
+    {
+        _container.Add<IComponentA, ComponentA>();
+        Assert.Throws<EntryPointNotFoundException>(() => _container.Get<IComponentA>());
+    }
+}
---
+++ TDD/1.param_parse/Container.cs
@@ -0,0 +1,32 @@
+﻿namespace _1.param_parse;
+
+public class Container
+{
+    private Dictionary<Type, Type> _dictionary = new();
+
+    public void Add<TService, TImp>()
+    {
+        _dictionary.Add(typeof(TService), typeof(TImp));
+    }
+
+    private Type Get(Type type)
+    {
+        if (_dictionary.TryGetValue(type, out var imp) == false)
+        {
+            throw new EntryPointNotFoundException();
+        }
+
+        return imp;
+    }
+
+    public T? Get<T>()
+    {
+        var imp = Get(typeof(T));
+        var constructors = imp.GetConstructors();
+        if (constructors.Length > 1) throw new ApplicationException("Too many constructors");
+        var constructor = constructors.First();
+        var parameters = constructor.GetParameters();
+        var parameterInstances = parameters.Select(p => Activator.CreateInstance(Get(p.ParameterType))).ToArray();
+        return (T?)constructor.Invoke(parameterInstances);
+    }
+}
