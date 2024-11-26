51233fa3311b6649c29964bad9c4298c5ec281ec
Zhi.Xin <zhi.xin@myob.com>
2024-11-26 15:19:09 +0800

    11.26

--- TDD/tdd.di/src/main/Context.java
+++ TDD/tdd.di/src/main/Context.java
@@ -2,12 +2,30 @@ package main;
 
 import test.ContainerTest;
 
-public class Context {
-    public <T> void bind(Class<T> type, T instance) {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Context {
+    private final Map<Class<?>, Object> components = new HashMap<>();
+    private final Map<Class<?>, Class<?>> componentImps = new HashMap<>();
+
+    public <T> void bind(Class<T> type, T instance) {
+        components.put(type, instance);
     }
 
-    public <T> Object get(Class<T> type) {
-        return null;
+    public <T, TImp extends T> void bind(Class<T> type, Class<TImp> imp) {
+        componentImps.put(type, imp);
+    }
+
+    public <T> T get(Class<T> type) {
+        if (components.containsKey(type)) {
+            return (T) components.get(type);
+        }
+        Class<?> imp = componentImps.get(type);
+        try {
+            return (T) imp.newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- TDD/tdd.di/src/test/ContainerTest.java
+++ TDD/tdd.di/src/test/ContainerTest.java
@@ -4,21 +4,40 @@ import main.Context;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
 public class ContainerTest {
-    interface Component{}
+    interface Component {
+    }
+
+    static class ComponentWithDefaultCtor implements Component {
+        public ComponentWithDefaultCtor() {
+        }
+    }
 
     @Nested
-    public class ComponentConstruction{
+    public class ComponentConstruction {
 
         @Test
-        public void should_bind_type_to_a_specific_instance(){
+        public void should_bind_type_to_a_specific_instance() {
             Context context = new Context();
-            Component component=new Component(){};
+            Component component = new Component() {
+            };
 
             context.bind(Component.class, component);
             assertSame(component, context.get(Component.class));
+        }
+
+        @Nested
+        public class ConstructorInjection {
+            @Test
+            public void should_bind_type_with_default_constructor() {
+                Context context = new Context();
+                context.bind(Component.class, ComponentWithDefaultCtor.class);
+                Component instance = context.get(Component.class);
+                assertNotNull(instance);
+            }
         }
     }
 }
