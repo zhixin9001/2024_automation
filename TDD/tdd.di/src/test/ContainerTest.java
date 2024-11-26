efa497c9dec8d214059189d75f5124da0160c092
zhixin <xin.zhi@thoughtworks.com>
2024-11-26 23:05:07 +0800

    11.26

--- TDD/tdd.di/src/test/ContainerTest.java
+++ TDD/tdd.di/src/test/ContainerTest.java
@@ -3,21 +3,13 @@ package test;
 import main.Context;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import test.utils.Component;
+import test.utils.ComponentWithDefaultCtor;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
-interface Component {
-}
-
-class ComponentWithDefaultCtor implements Component {
-    public ComponentWithDefaultCtor() {
-    }
-}
-
 public class ContainerTest {
-
-
     @Nested
     public class ComponentConstruction {
 
@@ -35,19 +27,12 @@ public class ContainerTest {
         public class ConstructorInjection {
             @Test
             public void should_bind_type_with_default_constructor() {
-                try {
-                    Class<?> cla = Class.forName("test.ComponentWithDefaultCtor");
-                    ComponentWithDefaultCtor a = (ComponentWithDefaultCtor) cla.getDeclaredConstructor().newInstance();
-                    assertNotNull(a);
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-
-//                Context context = new Context();
-//                context.bind(Component.class, ComponentWithDefaultCtor.class);
-//                Component instance = context.get(Component.class);
-//                assertNotNull(instance);
+                Context context = new Context();
+                context.bind(Component.class, ComponentWithDefaultCtor.class);
+                Component instance = context.get(Component.class);
+                assertNotNull(instance);
             }
         }
     }
 }
+
---
+++ TDD/tdd.di/src/test/utils/ComponentWithDefaultCtor.java
@@ -0,0 +1,4 @@
+package test.utils;
+
+public class ComponentWithDefaultCtor implements Component {
+}
